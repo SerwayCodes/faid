bad7bd15b867285eee13559f042119f117d48549
Synet Maonekedwe <serway@kali>
2024-09-28 01:49:41 +0200

    corousel static image

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import type { StaticImageData } from "next/image";
 import Image from "next/image";
 
+// Define the props interface
+interface CardProps {
+  imageSrc: StaticImageData; // Type for the image source
+  title: string; // Type for the title
+  description: string; // Type for the description
+}
+
 // Reusable card component
-const Card = ({ imageSrc, title, description }) => {
+const Card: React.FC<CardProps> = ({ imageSrc, title, description }) => {
   return (
-    <div className="bg-white shadow-md rounded-lg overflow-hidden w-33.333% md:w-1/5 m-4">
+    <div className="bg-white shadow-md rounded-lg overflow-hidden w-[33.333%] md:w-1/5 m-4">
       <Image src={imageSrc} alt={title} className="w-full h-58 object-cover" />
       <div className="p-6">
         <h2 className="text-xl font-bold text-green-600 mb-2">{title}</h2>
