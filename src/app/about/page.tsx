0a4b978362b7a5dc8a9e015957b6f4545214c7ac
Serway <smaonekedwe@gmail.com>
2024-10-10 15:47:03 +0200

    icon and color change on menu

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,8 +10,7 @@ import faid6 from "@/../public/images/Greens.jpg";
 import faid7 from "@/../public/images/Chitani.jpg";
 import faid8 from "@/../public/images/Stephen.jpg";
 import faid9 from "@/../public/images/synet.jpg";
-
-
+import faid10 from "@/../public/images/pumbwa.jpg";
 const AboutUs = () => {
   const teamMembers = [
     {
@@ -39,40 +38,49 @@ const AboutUs = () => {
       imageSrc: faid4, // Path to the image
     },
     {
-      name:"Innocent Kaphaso ",
-      role:"Financial Manager",
-      bio:"Innocent Kaphaso holds a Bachelor of Business Administration and a Bachelor of Computer Applications. He is currently serving as the Financial Manager at FAID",
-      imageSrc:faid5,
+      name: "Innocent Kaphaso ",
+      role: "Financial Manager",
+      bio: "Innocent Kaphaso holds a Bachelor of Business Administration and a Bachelor of Computer Applications. He is currently serving as the Financial Manager at FAID",
+      imageSrc: faid5,
     },
     {
-      name : "Greens Chapweteka",
-      role:"Deputy Programs Manager",
-      bio:"Greens Chapweteka currently serving as FAID Deputy Program manager.Born In Zimbabwe And now Malawi.Greens holds Diploma In Human Resources Management And In political philosophy",
-      imageSrc:faid6,
+      name: "Greens Chapweteka",
+      role: "Deputy Programs Manager",
+      bio: "Greens Chapweteka currently serving as FAID Deputy Program manager.Born In Zimbabwe And now Malawi.Greens holds Diploma In Human Resources Management And In political philosophy",
+      imageSrc: faid6,
     },
     {
-      name : "Chitani Tobias",
+      name: "Chitani Tobias",
       role: "Legal And Partnership affairs manager",
       bio: "Chitani Tobias is a second-year PhD student in Geoinformatics (GIS) at Mzuzu University. He holds a Master's in Geography and Environmental Management, along with a Bachelor's in Education in Humanities (Geography and Social Studies). Chitani is a lecturer at the University of Livingstonia and serves as a football referee in the Elite TNM Super League",
-      imageSrc:faid7,
+      imageSrc: faid7,
     },
     {
-      name:"Stephen Mathews",
-      role:"FAID Executive Secretary",
-      bio : "Stephen Matthews is currently serving as the Administrative Secretary and Deputy Human Resources Manager at FAID. He holds an Advanced Diploma in Business Administration and is one of the founding members of FAID.",
-      imageSrc:faid8
+      name: "Stephen Mathews",
+      role: "FAID Executive Secretary",
+      bio: "Stephen Matthews is currently serving as the Administrative Secretary and Deputy Human Resources Manager at FAID. He holds an Advanced Diploma in Business Administration and is one of the founding members of FAID.",
+      imageSrc: faid8,
+    },
+
+    {
+      name: "Charles Pumbwa",
+      role: "Faid Project Manager",
+      bio: "Bachelor's in Social Work, Project Management, Monitoring & Evaluation (DMI Mangochi) Diploma in Biomedical Engineering (Polytechnic/MUBAS) Diploma in Community Development (Soche Technical College) 10+ years in project management, community development, and biomedical engineering Worked with renowned organizations (Banja LA Mtsogolo, CADECOM, FHI, MSH, Airtel, Baylor College, PCI, Save the Children) Held key roles: Community Outreach Assistant, Project Monitoring Officer Project Management Monitoring & Evaluation Biomedical Engineering Community Development Communication Problem-Solving Dedicated professional committed to advancing community health and well-being through impactful projects. Proven track record of successful project management and passion for creating positive change. Available for collaboration opportunities and inquiries about community development work.",
+      imageSrc: faid10,
     },
     {
-      name:"Synet W Maonekedwe",
-      role:"ICT Manager",
+      name: "Synet W Maonekedwe",
+      role: "ICT Manager",
       bio: "Synet Maonekadwe is currently serving as the ICT Manager. He holds an Honours Bachelor's degree in Electronics and Computer Engineering",
-      imageSrc:faid9
-    }
+      imageSrc: faid9,
+    },
   ];
 
   return (
     <div className="max-w-7xl mx-auto">
-      <h1 className="text-3xl font-bold text-green-600 mb-6 text-center">About Us</h1>
+      <h1 className="text-3xl font-bold text-green-600 mb-6 text-center">
+        About Us
+      </h1>
       <p className="text-lg text-gray-700 mb-4 p-2">
         Future For All In Development (FAID) is a non-profit organization
         committed to promoting food security, self-reliance, and improved
@@ -81,8 +89,9 @@ const AboutUs = () => {
         through education, climate action, and livelihood improvements.
       </p>
 
-
-      <h2 className="text-2xl font-bold text-green-600 mb-6 text-center">Our Team</h2>
+      <h2 className="text-2xl font-bold text-green-600 mb-6 text-center">
+        Our Team
+      </h2>
       <div className="flex flex-wrap justify-center">
         {teamMembers.map((member, index) => (
           <ProfileCard
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,7 +32,7 @@ const Navbar = () => {
 
   return (
     <div className="p-3 mx-auto bg-green-600">
-      <div className="flex justify-evenly items-center  w-full h-20 px-2 text-white nav">
+      <div className="flex justify-between items-center w-full h-20 px-2 text-white">
         <div className="flex items-center">
           {/* Image logo from public/images/icon.png */}
           <Image
@@ -41,15 +41,11 @@ const Navbar = () => {
             width={70} // Set the desired width
             height={70} // Set the desired height
             className="rounded"
-            
           />
         </div>
         {/* Full Text for larger screens */}
         <div className="hidden md:block ml-2">
-          <h1 className="text-2xl font-bold">
-          FAID
-          </h1>
-          
+          <h1 className="text-2xl font-bold">FAID</h1>
         </div>
 
         {/* Short Text for smaller screens */}
@@ -61,17 +57,19 @@ const Navbar = () => {
           </p>
         </div>
 
+        {/* Desktop Links */}
         <ul className="hidden md:flex">
           {links.map(({ id, link, label }) => (
             <li
               key={id}
-              className="nav-links px-4 cursor-pointer capitalize font-medium text-white hover:scale-105 hover:text-grey-900 duration-200 link-underline"
+              className="nav-links px-4 cursor-pointer capitalize font-medium text-white hover:scale-105 hover:text-gray-900 duration-200"
             >
               <Link href={link}>{label}</Link>
             </li>
           ))}
         </ul>
 
+        {/* Mobile Hamburger Menu */}
         <div
           onClick={() => setNav(!nav)}
           className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
@@ -79,20 +77,23 @@ const Navbar = () => {
           {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
         </div>
 
-        {nav && (
-          <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
-            {links.map(({ id, link, label }) => (
-              <li
-                key={id}
-                className="px-4 cursor-pointer capitalize py-6 text-4xl"
-              >
-                <Link onClick={() => setNav(!nav)} href={link}>
-                  {label}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        )}
+        {/* Mobile Menu (sliding from the left) */}
+        <div
+          className={`fixed bg-gradient-to-r from-purple-500 to-pink-500 top-0 left-0 w-full h-screen  text-gray-500 flex flex-col justify-center items-center transition-transform duration-500 ease-out transform ${
+            nav ? "translate-x-0" : "-translate-x-full"
+          }`}
+        >
+          {links.map(({ id, link, label }) => (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize py-6 text-4xl"
+            >
+              <Link onClick={() => setNav(false)} href={link}>
+                {label}
+              </Link>
+            </li>
+          ))}
+        </div>
       </div>
     </div>
   );
